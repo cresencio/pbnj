dc20da049e0822472345572d313259679ff15c8d
Cresencio Cantu <cresenciocantu@gmail.com>
2023-08-24 11:42:38 -0500

    dont track changes and push to public repo

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const prompt = require('prompt');
-const gitClone = require('git-clone');
+const { exec } = require('child_process');
 
 const args = process.argv.slice(2);
 let projectName = args.find(arg => arg.startsWith('--name='))?.split('=')[1];
@@ -35,14 +35,23 @@ function createProject(name) {
     }
 
     console.log(`Cloning into ${name}...`);
-    gitClone('https://github.com/cresencio/pbnj', projectPath, {}, (err) => {
-      if (err) {
-        console.error('An error occurred while cloning the project:', err);
+    exec(`git clone --depth 1 https://github.com/cresencio/pbnj.git ${projectPath}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`An error occurred while cloning the project: ${error}`);
         return;
       }
 
-      console.log(`Project created: cd into ${name} and run npm install && npm run start`);
+      // Removing .git directory
+      exec(`rm -rf ${path.join(projectPath, '.git')}`, (err) => {
+        if (err) {
+          console.error(`An error occurred while removing the .git directory: ${err}`);
+          return;
+        }
+
+        console.log(`Project created: cd into ${name} and run npm install && npm run start`);
+      });
     });
+
   } catch (err) {
     console.error('An error occurred while creating the project:', err);
   }
