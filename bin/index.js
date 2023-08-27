f714ac8f9555c819aee8427a9a62e7969e5700ea
Cresencio Cantu <cresenciocantu@gmail.com>
2023-08-27 10:17:28 -0500

    add comments

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
+
+// Importing required modules
 const fs = require('fs');
 const path = require('path');
 const prompt = require('prompt');
 const { exec } = require('child_process');
 
+// Extracting command line arguments
 const args = process.argv.slice(2);
 let projectName = args.find(arg => arg.startsWith('--name='))?.split('=')[1];
 
+// Checking if project name is provided as a command line argument
 if (!projectName) {
+  // If project name is not provided, prompt the user to enter a project name
   prompt.start();
   prompt.get([{
     name: 'name',
@@ -19,21 +24,27 @@ if (!projectName) {
       return;
     }
 
+    // Call the createProject function with the entered project name
     createProject(result.name);
   });
 } else {
+  // If project name is provided as a command line argument, call the createProject function with the provided project name
   createProject(projectName);
 }
 
+// Function to create a new project
 function createProject(name) {
+  // Generating the project path by joining the current working directory and the project name
   const projectPath = path.join(process.cwd(), name);
 
   try {
+    // Checking if the project directory already exists
     if (fs.existsSync(projectPath)) {
       console.error(`Directory ${projectPath} already exists.`);
       return;
     }
 
+    // Cloning a git repository into the project path
     console.log(`Cloning into ${name}...`);
     exec(`git clone --depth 1 https://github.com/cresencio/pbnj.git ${projectPath}`, (error, stdout, stderr) => {
       if (error) {
@@ -41,13 +52,14 @@ function createProject(name) {
         return;
       }
 
-      // Removing .git directory
+      // Removing the .git directory from the cloned repository
       exec(`rm -rf ${path.join(projectPath, '.git')}`, (err) => {
         if (err) {
           console.error(`An error occurred while removing the .git directory: ${err}`);
           return;
         }
 
+        // Success message after project creation
         console.log(`Project created: cd into ${name} and run npm install && npm run start`);
       });
     });
